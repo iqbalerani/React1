18b9484d75b69590a834e40addc0e44ab327274d
Iqbal Erani <iqbalerani@gmail.com>
2023-02-20 18:33:06 +0500

    Commit 2

---
+++ index.js
@@ -0,0 +1,27 @@
+// const h1 = document.createElement('h1');
+
+// h1.textContent = "This is an imperative way to program";
+// h1.className = "header";
+// const root = document.getElementById("root")
+
+// const navBar = (
+//     <nav>
+//         <h1>Bob's Bistro</h1>
+//         <ul>
+//             <li>Menu</li>
+//             <li>About</li>
+//             <li>Contact</li>
+//         </ul>
+//     </nav>
+// )
+
+// const page = 
+//     <div>
+//         <h1 className="header">This is JSX</h1>
+//         <p>This is a paragraph</p>
+//         {navBar}
+//     </div>
+// console.log(page)
+
+// ReactDOM.render(page, root)
+
